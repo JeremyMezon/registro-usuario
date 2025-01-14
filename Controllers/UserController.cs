26ed3287fe1ce25d951789663535710e1be9905c
Jeremy Mezon de la Cruz <jeremymezon@gmail.com>
2025-01-14 19:25:11 -0400

    Init Proyect

---
+++ Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using registro_usuario.Interfaces;
+using registro_usuario.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace registro_usuario.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+        [HttpPost]
+        //[Route("User")]
+        public IActionResult RegisterUser([FromBody]User user)
+        {
+            return Created("/",new {message = "User Created"});
+        }
+    }
+}
+
---
+++ Models/Phone.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace registro_usuario.Models
+{
+	public class Phone
+	{
+		public string number { get; set; }
+		public string citycode { get; set; }
+		public string contrycode { get; set; }
+    }
+}
+
